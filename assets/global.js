1bfbe9ab53bee9a7cd697d11e2c5709c7df57952
Kevin O'Neill <kevinallenoneill@gmail.com>
2022-04-06 21:17:48 -0500

    changed header css and added logo

--- assets/global.js
+++ assets/global.js
@@ -7,10 +7,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // Functions
     const toggleMenu = () => {
         menu_nav.classList.toggle("active");
+        menu_icon.classList.toggle("active");
+        if(menu_nav.classList.contains("closed")) {
+            menu_nav.classList.remove("closed");
+        } else {
+            setTimeout(() => {
+                menu_nav.classList.add("closed");
+            }, 200);
+        }
     }
 
     // Open and close menu
     menu_icon.addEventListener("click", toggleMenu);
-    
-
 })
